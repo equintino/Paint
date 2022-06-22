233977e62e3df87ea0bab2517e5b0d12786d07d2
edmilson <emquintino@gmail.com>
2022-06-22 12:44:23 -0300

    fix: changed permition and namespace

--- exemple.php
+++ exemple.php
@@ -1,12 +1,22 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
-$image = new Paint\Image('tests/carlos.jpeg');
-$image->addTransformation(new Transformation\Crop(300, 200));
-$image->addFilter(new Paint\Filter\FilterGrayscale());
-$image->generate(new Paint\Format\JPEG(60), 'tests/destination.jpeg');
+use Paint\Image;
+use Paint\ImageFactory;
+use Paint\Filter\Grayscale;
+use Paint\Format\JPEG;
+use Paint\Transformation\Crop;
+
+$image = new Image('tests/carlos.jpeg');
+$image->addTransformation(new Crop(300, 200));
+$image->addFilter(new Grayscale());
+$image->generate(new JPEG(60), 'tests/destination.jpeg');
+$img = "tests/destination.jpeg";
+echo "<img src='{$img}' alt='result' />";
 
 $factory = ImageFactory::create('tests/carlos.jpeg');
-$factory->addTransformation(new Transformation\Crop(300, 200));
-$factory->addFilter(new Filter\FilterGrayscale());
+$factory->addTransformation(new Crop(300, 200));
+$factory->addFilter(new Grayscale());
 $factory->generate('jpeg', 'tests/destination.jpg');
+$img = "tests/destination.jpg";
+echo "<img src='{$img}' alt='result' />";
